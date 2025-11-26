23d8134affa140db6fc4c6ceead73cccf93ea6f2
Bryan <trome12@gmail.com>
2025-11-26 16:22:09 -0500

    cambios en metodo metopagocontrolles, metodopago, orden controller,, usuarios controller

--- routes/MetodosPagos.js
+++ routes/MetodosPagos.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { asignarMetodoPago, obtenerMetodosPorUsuario } = require('../controllers/metodosPagoController');
+const { asignarMetodoPago, obtenerMetodosPorUsuario, editarMetodoPago, eliminarMetodoPago } = require('../controllers/metodosPagoController');
 
+// 👇 IMPORTANTE: Importa y usa tu middleware aquí
+const authMiddleware = require('../middleware/authMiddleware');
+router.use(authMiddleware); 
+
+// Rutas
 router.post('/asignar', asignarMetodoPago);
 router.get('/usuario/:id', obtenerMetodosPorUsuario);
+router.put('/:id', editarMetodoPago);
+router.delete('/:id', eliminarMetodoPago);
 
 module.exports = router;
