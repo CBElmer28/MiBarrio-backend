fc9a1847d5ea1947708a8cbf16a5bffcb2a0ce7b
CBElmer28 <cbelmer28@gmail.com>
2025-11-26 19:21:05 -0500

    feat(database): Cambio de mysql a postgresql, para despliegue - Cambio en database.js - Cambio en package.json

--- config/database.js
+++ config/database.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
   logging: false,
   define:{
     underscored: true
